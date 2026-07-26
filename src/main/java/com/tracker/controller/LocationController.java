c762b548e99cc4a9f18e582b4a67d005bcaa5eec
Ashok G <ashok.gurunathan@calix.com>
2026-07-27 00:03:16 +0530

    Resolving CORS issue

--- src/main/java/com/tracker/controller/LocationController.java
+++ src/main/java/com/tracker/controller/LocationController.java
@@ -2,6 +2,7 @@ package com.tracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/locations")
 public class LocationController {
 
